afbdfd16414d66bc46f6e5fcb6a7d07e3295f965
cristian1997 <cristian.vintur@gmail.com>
2019-03-24 22:40:35 +0200

    final template

--- content/contest/templateAlex.cpp
+++ content/contest/templateAlex.cpp
@@ -6,11 +6,14 @@
 #define dbg_v(x, n) do{cerr<<#x"[]: ";for(int _=0;_<n;++_)cerr<<x[_]<<" ";cerr<<'\n';}while(0)
 #define dbg_ok cerr<<"OK!\n"
 
+#define all(v) v.begin(), v.end()
 #define st first
 #define nd second
 
 // using namespace __gnu_pbds; 
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
 // template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // ordered_set <int> s;
 template<class T> ostream& prnt(ostream& out, T v) { out << v.size() << '\n'; for(auto e : v) out << e << ' '; return out;}
@@ -19,12 +22,8 @@ template<class T> ostream& operator<<(ostream& out, set <T> v) { return prnt(out
 template<class T1, class T2> ostream& operator<<(ostream& out, map <T1, T2> v) { return prnt(out, v); }
 template<class T1, class T2> ostream& operator<<(ostream& out, pair<T1, T2> p) { return out << '(' << p.st << ' ' << p.nd << ')'; }
 
-const int N = ;
-long long n, k, m;
-
 int main() {
 	ios_base::sync_with_stdio(false);
-
-	cin >> n;
+	return 0;
 }	
 
