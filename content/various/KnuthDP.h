407617b16461c6be7e12dbfa174098565ada7b33
alexandru <alexandru@T430>
2019-03-25 21:37:41 +0200

    removed math formulas

--- content/various/KnuthDP.h
+++ content/various/KnuthDP.h
@@ -7,11 +7,8 @@
  *  This is known as Knuth DP. Sufficient criteria for this are if $f(b,c) \le f(a,d)$ and $f(a,c) + f(b,d) \le f(a,d) + f(b,c)$ for all $a \le b \le c \le d$.
  *  Consider also: LineContainer (ch. Data structures), monotone queues, ternary search.
  *
- * dp[i] = min j<i {dp[j] + b[j] * a[i]} when b[j] >= b[j+1], a[i] <= a[i+1]: O(n^2) -> O(n)
- *
- * dp[i][j] = min k<j {dp[i-1][k] + b[k] * a[j]} when b[k] >= b[k+1], a[j] <= a[j+1]: O(kn^2) -> O(kn)
- *
- * dp[i][j] = min k<j {dp[i-1][k] + C[k][j]}, optim[i][j] <= optim[i][j+1]: O(kn^2) -> O(kn logn)
- *
+ * $dp[i] = min j<i {dp[j] + b[j] * a[i]} when b[j] >= b[j+1], a[i] <= a[i+1]: O(n^2) -> O(n)$ \\
+ * $dp[i][j] = min k<j {dp[i-1][k] + b[k] * a[j]} when b[k] >= b[k+1], a[j] <= a[j+1]: O(kn^2) -> O(kn)$ \\
+ * $dp[i][j] = min k<j {dp[i-1][k] + C[k][j]}, optim[i][j] <= optim[i][j+1]: O(kn^2) -> O(kn logn)$ \\
  * Time: O(N^2)
  */
